9ba63e70d0c27c5e7e6864993980250cf6253318
desktop_10400 <web_89356484649@yahoo.co.jp>
2023-06-25 01:35:03 +0900

    202306250135
    uploaded at home

--- mix_fitlog/fitlog_mixer.cpp
+++ mix_fitlog/fitlog_mixer.cpp
@@ -14,6 +14,7 @@ int main(int argc, char* argv[]){
         return 0;
     }
     sprintf(comand, "cd %s", argv[1]);
+//    printf("cd %s\n", argv[1]);
     system(comand);
 
     pipe = _popen("dir *.fitlog", "r");
@@ -41,83 +42,42 @@ int main(int argc, char* argv[]){
     out_file = fopen("fit_param.dat", "w");
     char file_name[256];
 
+    for(int i = 0; i < 256; i++){
+        file_name[i] = '\0';
+        buf[i] = '\0';
+    }
+
     // write output file
+    int char_count = 0;
+    char colum[14][256];
+    for(int i = 0; i < 14; i++){
+        for(int j = 0; j < 256; j++){
+            colum[i][j] = '\0';
+        }
+    }
     for(int i = 0; i < file_count; i++){
 
         getc(stdin);
 
         sprintf(file_name, "%d.fitlog\0", i + 1);
-        log_file = fopen(file_name, "w");
+        log_file = fopen(file_name, "r");
+
 
         if(log_file == NULL){
-            printf("failed open logfile : %s", file_name);
+            printf("failed open logfile : %s\n", file_name);
             return 0;
         }
         else {
-            printf("open log file : %s", file_name);
+            //printf("open log file : %s\n", file_name);
         }
 
-        char buf;
-
-        while(!feof(log_file)){
-            buf = getc(log_file);
-            fprintf(out_file, "%d", (int)buf);
-            fprintf(stdout, "%d", (int)buf);
-            getc(stdin);
-
+        for(int j = 0; j < 256; j++){
+            buf[j] = '\0';
         }
 
-        printf("finish log file : %s", file_name);
-
-/*
-
-        // set fitlog file name
-        sprintf(file_name, "%d.fitlog", i + 1);
-
-        //open fitlog
-        log_file = fopen(file_name, "r");
-        
+        //ignor lines start with '#'
         int count = 0;
-        while(!feof(log_file)){
-
-            fgets(buf, sizeof(buf), log_file);
-
-            //ignor lines start with '#'
-            if(*(buf) == '#'){
-                continue;
-            }
-
-            //write numbers
-
-            //file count
-            fprintf(out_file, "%d", i + 1);
-
-            //looking for start of numbers
-            count = 0;
-            while(1){
-                if(*(buf + count) == '='){
-                    count++;
-                    break;
-                }
-                count++;
-            }
-
-            //write numberes
-            while(1){
-                if(*(buf + count) == '/n'){
-                    fputc('\t', out_file);
-                }
-                putc(*(buf + count), out_file);
-                count++;
-            }
-
-        }
-
-        fputc('\n', out_file);
-        fclose(log_file);
-
-    */
-
+        
     }
     fclose(out_file);
 
