81c9d8aff155cc0f66a18bd253090c381878ed81
kojima_lab <larc.hk2000.4649@gmail.com>
2024-07-22 15:22:09 +0900

    ''

--- Miller-Rabin/MillerRabin/Program.cs
+++ Miller-Rabin/MillerRabin/Program.cs
@@ -9,20 +9,6 @@ namespace MillerRabin
         public static void Run()
         {
 
-            for(int i = 0; i < 65; i++)
-            {
-                MulitDigitNumber n = new MulitDigitNumber(i);
-
-                Console.WriteLine($"{i} : {n.byteLen()}");
-            }
-
-            byte[] bytes = new byte[3];
-            bytes[0] = byte.Parse("255");
-            bytes[1] = byte.Parse("0");
-            bytes[2]= byte.Parse("255");
-            MulitDigitNumber m = new MulitDigitNumber(bytes);
-            Console.WriteLine($"m length : {m.byteLen()}");
-            m.showBytes();
 
 
         }
@@ -32,80 +18,77 @@ namespace MillerRabin
         }
     }
 
+    public class UndefinedArray
+    {
+        private static UndefinedArray? _Instance = null;
+        private static ushort[]? arrayRef;
+
+        public ushort[] ARRAYREF
+        {
+            get { return arrayRef; }
+        }
+
+        private UndefinedArray()
+        {
+
+        }
+
+        public static UndefinedArray Instance()
+        {
+            if(_Instance == null)
+            {
+                _Instance = new UndefinedArray();
+                arrayRef = new ushort[0];
+            }
+            
+            return _Instance;
+        }
+
+
+    }
+
     class MulitDigitNumber
     {
-
+        private ushort[] num;             //multi bit number (little endian)
         private int digit;
-        private byte[]? bytes;
 
-         #region constructor
-       public MulitDigitNumber(int digit)
+        #region constructor
+
+        public MulitDigitNumber(ushort[] number, bool isBigEndian)      //create multi digit number as big endian
         {
-            if(digit <= 0)
-            {
-                this.digit = 0;
-                this.bytes = null;
 
-                Console.WriteLine("Digit must lager than 0.");
-            }
-            else if(digit % 8 != 0)
+            if(isBigEndian)     //if number is big endian, convert to little endian.
             {
-                this.digit = 0;
-                this.bytes = null;
+                convertEndian(number);
+            }
 
-                Console.WriteLine("Digit must be multiple of octed");
-            }
-            else 
+            num = new ushort[number.Length];            //make deep copy
+            for (int i = 0; i < number.Length; i++)
             {
-                this.digit = digit;
-                this.bytes = new byte[digit / 8];
+                num[i] = number[i];
             }
+
+
 
         }
-        public MulitDigitNumber(int digit, byte[] bytes) : this(digit)
-        {
-            if(digit != bytes.Length)
-            {
-                this.digit = 0;
-                this.bytes = null;
-                Console.WriteLine("byte length doesn't match to digit.");
-            }
-            else
-            {
-                this.digit = digit;
-                this.bytes = new byte[this.digit];
-                for(int i = 0; i < digit; i++)
-                {
-                    this.bytes[i] = bytes[i];
-                }
-            }
-        }
-        public MulitDigitNumber(byte[] bytes)
-        {
-            this.digit = bytes.Length;
-            this.bytes = new byte[this.digit];
-            for (int i = 0; i < digit; i++)
-            {
-                this.bytes[i] = bytes[i];
-            }
-        }
+
+
         #endregion
 
-        public int byteLen()
-        {
-            if(digit == 0)
-            {
-                return 0;
-            }
-            else
-            {
-                return bytes.Length;
-            }            
-        }
 
-        public void showBytes()
+
+        public static void convertEndian(ushort[] obj)      //convert little endian and big endian
         {
-            Console.WriteLine($"bytes : {}");
+            ushort[] tmp = new ushort[obj.Length];
+
+            for(int i = 0; i < tmp.Length; i++)
+            {
+                tmp[i] = obj[i];
+            }
+            for(int i = 0; i <tmp.Length; i++)
+            {
+                obj[i] = tmp[tmp.Length - i];
+            }
         }
 
 
